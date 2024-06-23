86ffcf50cf08aa2810ebeaa692c52e804074ab4a
BiYeon <isly7959@naver.com>
2024-06-23 21:56:32 +0900

    feat: UserDTO 및 User테이블 생성

---
+++ src/main/java/com/example/hearurbackend/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.example.hearurbackend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserDTO {
+    private String role;
+    private String name;
+    private String username;
+}
---
+++ src/main/java/com/example/hearurbackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.hearurbackend.repository;
+
+import com.example.hearurbackend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
