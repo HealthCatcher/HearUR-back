54bb0e8d20feb7f4ef1b14e3d8cdac8ddc7c1292
byun0-0 <isly7959@naver.com>
2024-07-02 21:46:13 +0900

    feat: jwt 위치 쿠키 -> 헤더 로직 구현

--- src/main/java/com/example/hearurbackend/jwt/JWTFilter.java
+++ src/main/java/com/example/hearurbackend/jwt/JWTFilter.java
@@ -27,28 +27,36 @@ public class JWTFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain
     ) throws ServletException, IOException {
+
         //cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
-        String authorization = null;
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
             filterChain.doFilter(request, response);
             //조건이 해당되면 메소드 종료 (필수)
             return;
         }
-        for (Cookie cookie : cookies) {
-            System.out.println(cookie.getName());
-            if (cookie.getName().equals("Authorization")) {
-                authorization = cookie.getValue();
+
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null) {
+            authorization = authorization.split(" ")[1];
+            logger.info("Authorization 헤더가 발견되었습니다.");
+        } else {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("Authorization")) {
+                    authorization = cookie.getValue();
+                    logger.info("jwt가 쿠키로부터 발견되었습니다.");
+                    break;
+                }
+            }
+            //Authorization 헤더 검증
+            if (authorization == null) {
+                logger.info("쿠키에 Authorization이 없습니다.");
+                filterChain.doFilter(request, response);
+                //조건이 해당되면 메소드 종료 (필수)
+                return;
             }
         }
 
-        //Authorization 헤더 검증
-        if (authorization == null) {
-            System.out.println("token null");
-            filterChain.doFilter(request, response);
-            //조건이 해당되면 메소드 종료 (필수)
-            return;
-        }
         //토큰
         String token = authorization;
 
