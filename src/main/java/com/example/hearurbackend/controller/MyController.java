5558a42fc6bf49fcf88266e63526ae7ff866ca25
byun0-0 <isly7959@naver.com>
2024-07-05 01:12:09 +0900

    feat: authentcation 확인 용 컨트롤러 추가

--- src/main/java/com/example/hearurbackend/controller/MyController.java
+++ src/main/java/com/example/hearurbackend/controller/MyController.java
@@ -1,12 +1,20 @@
 package com.example.hearurbackend.controller;
 
+import com.example.hearurbackend.dto.CustomOAuth2User;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
+
+    @Operation(summary = "내 정보")
     @GetMapping("/my")
-    public String myAPI(){
-        return "my route";
+    public ResponseEntity<String> myAPI(@AuthenticationPrincipal CustomOAuth2User auth){
+
+        return ResponseEntity.ok(auth.getUsername());
     }
 }
