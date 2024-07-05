2de567e1902b63e6a31418327e39e78b7a0d151d
byun0-0 <isly7959@naver.com>
2024-07-05 13:01:32 +0900

    style: 람다식으로 변경

--- src/main/java/com/example/hearurbackend/dto/CustomOAuth2User.java
+++ src/main/java/com/example/hearurbackend/dto/CustomOAuth2User.java
@@ -26,12 +26,7 @@ public class CustomOAuth2User implements OAuth2User {
 
         Collection<GrantedAuthority> collection = new ArrayList<>();
 
-        collection.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return userDTO.getRole();
-            }
-        });
+        collection.add((GrantedAuthority) () -> userDTO.getRole().toString());
 
         return collection;
     }
