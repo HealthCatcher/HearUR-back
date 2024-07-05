44d6d941bde04ec02d9725c1b43233d31c65b301
byun0-0 <isly7959@naver.com>
2024-07-05 20:23:56 +0900

    feat: 회원 가입 및 비밀번호 변경 로직 auth controller 로 이동

--- src/main/java/com/example/hearurbackend/controller/UserController.java
+++ src/main/java/com/example/hearurbackend/controller/UserController.java
@@ -14,34 +14,7 @@ import java.net.URI;
 @RestController
 public class UserController {
     private final UserService userService;
-
     public UserController(UserService userService) {
         this.userService = userService;
-    }
-
-    @Operation(summary = "회원가입")
-    @PostMapping("/auth/signup")
-    public ResponseEntity<String> register(
-            @RequestBody UserDTO userDTO
-    ) {
-        try {
-            User newUser = userService.registerUser(userDTO);
-            return ResponseEntity.created(URI.create("/users/"+newUser.getUsername())).build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @Operation(summary = "비밀번호 변경")
-    @PostMapping("/auth/password")
-    public ResponseEntity<String> changePassword(
-            @RequestBody UserDTO userDTO
-    ) {
-        try {
-            userService.changePassword(userDTO);
-            return ResponseEntity.noContent().build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
     }
 }
