2ad9a934608236940579cfcc501cb64f89df289b
byun0-0 <isly7959@naver.com>
2024-07-09 02:22:17 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/example/hearurbackend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/hearurbackend/handler/GlobalExceptionHandler.java
@@ -3,16 +3,9 @@ package com.example.hearurbackend.handler;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
-
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
